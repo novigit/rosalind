b53171ed86ef93d924cc4837cfcbdf10664c8c63
novibit <joranmartyn@gmail.com>
2015-11-15 13:58:14 +0100

    finish revc problem

--- revc.pl
+++ revc.pl
@@ -3,3 +3,23 @@ use strictures;
 
 # take file from command line
 my ($file) = @ARGV;
+
+# open file
+open FILE, '<', $file;
+
+# data structures
+my ($dna, $c_dna, $rc_dna);
+
+# read dna sequence in file
+while (my $line = <FILE>){
+    chomp $line; # remove newline character from $line
+    $dna .= uc($line); # $dna should be all uppercase characters
+}
+
+# reverse complement dna
+# first complement
+($c_dna = $dna) =~ tr/ATGC/TACG/;
+# then reverse
+$rc_dna = reverse($c_dna);
+
+print STDOUT $rc_dna, "\n";
